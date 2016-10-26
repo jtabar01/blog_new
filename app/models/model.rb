a1b19757821c4881951eeb2f4e76a8871b3f9483
jtabar01 <jtabar01@unm.edu>
2016-10-26 09:10:45 -0600

    attempt for authentication

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,11 +1,6 @@
-class User < ActiveRecord::Base
-  rolify
+class Model < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :posts
-  has_many :comments
-  validates :first_name, presence: true
-  validates :last_name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :models
+  devise_for :users
   root 'posts#index'
   resources :users
   resources :comments
