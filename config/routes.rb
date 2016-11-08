933aa11036b36248e6ab9e884c6b70bd9c0868d1
jtabar01 <jtabar01@unm.edu>
2016-11-08 15:09:28 -0700

    fix login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, :path => '', :path_names => {:sign_up => 'register', :sign_in => 'login', :sign_out => 'logout'}
   root 'posts#index'
   resources :users
   resources :comments
